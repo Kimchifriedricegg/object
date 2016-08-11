cb00e7952f66bdef508ff53b4b567d6e9e161cdf
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-11 15:22:44 -0400

    added two instances of eat at method

--- cat.rb
+++ cat.rb
@@ -8,7 +8,21 @@ attr_accessor :name, :preferred_food, :meal_time, :meow
     @meal_time = meal_time
   end
 
+#added instance method eats at
+  def eats_at
+  if @meal_time >= 13
+    return "#{@meal_time - 12}pm"
+  elsif @meal_time <= 11
+    return "#{@meal_time}am"
+  else
+    return "#{@meal_time}pm"
+  end
+  end
 
 # added two instances of class
 cat1= Cat.new("Ahri", "apples", 14)
 cat2= Cat.new("Yasuo", "teemo", 1)
+
+# added two instances using eats at
+cat1.eats_at
+cat2.eats_at
