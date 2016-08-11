22c43e31c7277cde75e347fab5d99d8c104a56a7
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-11 15:21:27 -0400

    added two instances of cat class

--- cat.rb
+++ cat.rb
@@ -1,8 +1,14 @@
 class Cat
 
+attr_accessor :name, :preferred_food, :meal_time, :meow
+
   def initialize(name,preferred_food,meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
-end
+
+
+# added two instances of class
+cat1= Cat.new("Ahri", "apples", 14)
+cat2= Cat.new("Yasuo", "teemo", 1)
